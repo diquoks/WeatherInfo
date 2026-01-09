9d680a969e4c7fa728c36129ece38513c8788a73
Denis Titovets <den232titovets@yandex.ru>
2026-01-09 19:38:18 +0300

    Небольшой рефакторинг

--- src/main.cpp
+++ src/main.cpp
@@ -1,17 +1,18 @@
 #include <Arduino.h>
 #include <DHT.h>
-#include <Rtc_Pcf8563.h>
 #include <LiquidCrystal.h>
+#include <Rtc_Pcf8563.h>
 
 DHT sensor(8, DHT11);
 Rtc_Pcf8563 rtc;
 LiquidCrystal lcd(7, 6, 5, 4, 3, 2);
 
 void setup() {
-    // TODO: при первом запуске установить время, а затем закомментировать
+    // TODO: set on first startup
     // rtc.initClock();
     // rtc.setDate(29, 1, 7, false, 25);
     // rtc.setTime(0, 0, 0);
+
     lcd.begin(16, 2);
 
     Serial.begin(115200);
@@ -20,8 +21,8 @@ void setup() {
 void loop() {
     delay(1000);
 
-    const int humidity = int(sensor.readHumidity());
-    const int temperature = int(sensor.readTemperature());
+    const float humidity = sensor.readHumidity();
+    const float temperature = sensor.readTemperature();
 
     if (isnan(humidity) || isnan(temperature)) {
         Serial.println("An error occurred!");
@@ -30,21 +31,21 @@ void loop() {
         return;
     }
 
-    String humidity_string = "Hum:  $HUM%";
-    String temperature_string = "Temp: $TEMPC";
+    char humidity_char[16];
+    char temperature_char[16];
 
-    humidity_string.replace("$HUM", String(humidity));
-    temperature_string.replace("$TEMP", String(temperature));
+    sprintf(humidity_char, "Hum:  %ld%%", round(humidity)); // NOLINT(*-incorrect-roundings)
+    sprintf(temperature_char, "Temp: %ldC", round(temperature)); // NOLINT(*-incorrect-roundings)
 
-    Serial.println(humidity_string);
-    Serial.println(temperature_string);
+    Serial.println(humidity_char);
+    Serial.println(temperature_char);
 
     lcd.home();
-    lcd.print(humidity_string);
+    lcd.print(humidity_char);
     lcd.setCursor(11, 0);
     lcd.print(rtc.formatTime(2));
     lcd.setCursor(0, 1);
-    lcd.print(temperature_string);
+    lcd.print(temperature_char);
     lcd.setCursor(11, 1);
     lcd.print(rtc.formatDate());
 }
